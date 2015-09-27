6bc3cb214c94562cbaa6210dd3f79a50aeec3f2e
John <johnm589@yahoo.com>
2015-09-27 12:08:26 -0700

    Part 2

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -22,26 +22,57 @@ function randomPlay() {
 ////////////////////////////////////////////////
 
 function getPlayerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    if (move ! = null) }
+        return = move}
+    else {
+        return = getInput ()
+    }
+    
 }
 
 function getComputerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
-}
+    if (move ! = null)}
+        return = move }
+    else {
+        return = randomPlay ()
+    }
 
 function getWinner(playerMove,computerMove) {
     var winner;
-    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
-    // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
-    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
-    return winner;
+if (playerMove === 'rock' && computerMove==='paper') 
+    {
+        return 'You lose! Good Day!';
+}
+else if 
+    (playerMove==='rock' && computerMove ==='scissors')
+{return 'You Won!';
+}
+else if 
+(playerMove ==='scissors' && computerMove==='rock')
+{
+    result ==='You Won!';
+}
+else if
+ (playerMove==='scissors' && computerMove==='paper')
+{
+    result ==='You lose! Good day!'
+}
+else if 
+(playerMove ==='paper' && computerMove==='scissors')
+{
+    result ==='You lose! Good day!'
+}
+else if 
+(playerMove==='paper' && computerMovie==='rock')
+{
+    result === 'You Won!'
+}
+else if 
+    (playerMove===computerMove)
+{
+    result ==='tie'
+}
+
 }
 
 function playToFive() {
